1b6ff9d6e76052a9e242e322720092bc8d88a8e9
Kavin Charles <kavincharles@gmail.com>
2026-06-01 13:36:47 +0530

    chore(plugin-runtime): move to Vantage main monorepo

--- packages/plugin-runtime/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { checkPermission } from './permissions';
-export { dispatchBridgeCall } from './bridge-router';
-export type { BridgeContext } from './bridge-router';
-export { slugify, physicalTableName, dispatchTableCall } from './table-client';
-export { runMigrations, rollbackMigrations, ensureMigrationLog } from './migration-runner';
